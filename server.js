93329f9514bb32ea5547960c08210d17f0b3f05d
Nathan Huber <Nathan_Huber@yahoo.com>
2020-12-02 02:05:55 -0600

    Nodemailer is working

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const app = express();
 const path = require('path');
+const { send } = require('process');
 
 const host = process.env.HOST;
 const user = process.env.USER;
@@ -13,6 +14,34 @@ const PORT = process.env.PORT || 10001;
 //------------- Data Parsing ------------------------//
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.post('/email', (req, res) => {
+    const { email, fullName, message } = req.body;
+    console.log('Data: ', req.body);
+
+    const output = `
+  <p> You have a new contact request</p>
+  <h3>Contact Details</h3>
+  <ul>
+    <li>Name: ${req.body.fullName}</li> 
+    <li>Email: ${req.body.email} </li>
+  </ul>
+    <h3>Message:</h3>
+    <p> ${req.body.message} </p>
+    `;
+
+//   console.log(req.body);  
+
+
+  sendMail(email, fullName, message, function(err, data) {
+      if(err) {
+          console.log("Error: ", err);
+          return res.status(500).json({ message: err.message || "Internal Error" });
+      }
+      console.log("Email Sent!");
+      return res.json({ message: "Email Sent!" });
+  });
+});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -34,22 +63,31 @@ let transporter = nodemailer.createTransport({
     });
 
 
-let mailOptions = {
-    from: '"Nodemailer Contact 👻" <nathan_huber@comcast.net>', // sender address
-    to: "nathan_huber@yahoo.com, nathan_huber@comcast.net, ChicagoN8@Gmail.com", // list of receivers
-    subject: "Node Contact Request", // Subject line
-    text: "Hello world?", // plain text body
-    // html: output, // html body
-  };
 
-
-transporter.sendMail(mailOptions, function(err, data) {
-    if (err) {
-        console.log('Error Occured', err);
-    } else {
-        console.log('Email Sent!');
-    }
-});
+const sendMail = (email, fullName,  message, cb) => {
+    const mailOptions = {
+        from: "🎧 DJ Dan Website 🎧" + email,  // sender address
+        to: "nathan_huber@yahoo.com, nathan_huber@comcast.net, ChicagoN8@Gmail.com", // list of receivers
+        subject: fullName, // Subject line
+        text: message, // plain text body
+        html: `
+        <p> You have a new contact request</p>
+        <h3>Contact Details</h3>
+        <ul>
+          <li>Name: ${fullName} </li> 
+          <li>Email: ${email} </li>
+        </ul>
+          <h3>Message:</h3>
+          <p> ${message} </p>
+          ` // html body
+    };
+    transporter.sendMail(mailOptions, function(err, data) {
+        if (err) {
+            return cb(err, null);
+        }
+        return cb(null, data);
+    });
+}
 
 app.listen(PORT, () => console.log(`Server has started on port  ${PORT}!`));
 
